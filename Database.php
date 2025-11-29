a107092a21eb00e2b032b8479763130815155331
Camberden <42500247+Camberden@users.noreply.github.com>
2025-11-29 09:14:35 -0500

    PDO refactor; break needed

--- Database.php
+++ Database.php
@@ -4,9 +4,17 @@ class Database {
 
 	public $connection;
 	public function __construct() { 
+		$config = [
+			"host" => "localhost",
+			"port" => 3306,
+			"dbname" => "Blogging",
+			"charset" => "utf8mb4",
+		];
+
 		// Called on initialization!
-		$dsn = "mysql:host=localhost;port=3306;dbname=Blogging;charset=utf8mb4";
-		$this->connection = new PDO($dsn, "root"); //Data Source Name
+		// $dsn = `mysql:host={$config["host"]};port={$config["port"]};dbname={$config["dbname"]};charset={$config["charset"]}`;
+		$dsn = "mysql:" . http_build_query($config, "", ";");
+		$this->connection = new PDO($dsn, "root", "", [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]); //Data Source Name
 	}
 	public function query($query) {
 
--- index.php
+++ index.php
@@ -13,20 +13,5 @@ $onePost = $db->query("SELECT * FROM posts WHERE id = 1")->fetch(PDO::FETCH_ASSO
 foreach ($posts as $post) {
 	echo "<li>" . $post["title"] . "</li>";
 }
-
 echo "<code>" . $onePost["title"] . "</code>";
 
-/*
-class Person {
-	public $name;
-	public $age;
-	
-	public function breathe() {
-		echo $this->name . " is present and accounted for!";
-	}
-}
-$person = new Person();
-$person->name = "Chrispy Camberden";
-$person->age = 25;
-dumpAndDie($person->breathe());
-*/
