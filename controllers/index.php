99962abecda95b918db02ba00fcfd923f4a6a921
Camberden <42500247+Camberden@users.noreply.github.com>
2025-12-09 18:24:11 -0500

    minor tweaks; leetcode dabbling

--- controllers/index.php
+++ controllers/index.php
@@ -1,9 +1,14 @@
 <?php
 
-
-// dd($_SESSION);
-// $_SESSION["name"] = "Chrispy";
-// $_SESSION["last"] = "Camberden";
+function strThing($haystack, $needle) {
+	// if (str_contains($haystack, $needle)) {
+	// 	return strpos($haystack, $needle);
+	// } else {
+	// 	return -1;
+	// }
+	$result = str_contains($haystack, $needle) ? strpos($haystack, $needle) : -1;
+	return $result;
+}
 
 view("index.view.php", [
 	"heading" => "ほめぱげ",
